a4e90342386b1157d49c20f43c5dc5c8734e6247
CoderJT-Elite <jg.tewolde@gmail.com>
2026-04-03 11:23:28 -0400

    iterations of tof?
    just something we should test, latency compensation (delay from rio) and iterations of time of flight.

---
+++ src/main/java/frc/robot/Commands/Align/AlignOnTheMoveNew.java
@@ -0,0 +1,97 @@
+package frc.robot.Commands.Align;
+
+import static edu.wpi.first.units.Units.MetersPerSecond;
+import java.util.function.DoubleSupplier;
+import edu.wpi.first.math.MathUtil;
+import edu.wpi.first.math.controller.ProfiledPIDController;
+import edu.wpi.first.math.geometry.Pose2d;
+import edu.wpi.first.math.geometry.Rotation2d;
+import edu.wpi.first.math.geometry.Translation2d;
+import edu.wpi.first.math.kinematics.ChassisSpeeds;
+import edu.wpi.first.math.trajectory.TrapezoidProfile;
+import edu.wpi.first.wpilibj.DriverStation;
+import edu.wpi.first.wpilibj.DriverStation.Alliance;
+import edu.wpi.first.wpilibj2.command.Command;
+import com.ctre.phoenix6.swerve.SwerveRequest;
+import frc.robot.Constants.AlignConstants;
+import frc.robot.Constants.EquationConstants;
+import frc.robot.Constants.FieldConstants;
+import frc.robot.generated.TunerConstants;
+import frc.robot.subsystems.CommandSwerveDrivetrain;
+
+public class AlignOnTheMoveNew extends Command {
+  private final CommandSwerveDrivetrain drivetrain;
+  private final ProfiledPIDController thetaController;
+  private final DoubleSupplier xSupplier, ySupplier;
+  private final SwerveRequest.ApplyRobotSpeeds request = new SwerveRequest.ApplyRobotSpeeds();
+
+  public static double vGoalDist;
+
+  public AlignOnTheMoveNew(CommandSwerveDrivetrain drivetrain, DoubleSupplier x, DoubleSupplier y) {
+    this.drivetrain = drivetrain;
+    this.xSupplier = x;
+    this.ySupplier = y;
+
+    this.thetaController = new ProfiledPIDController(
+        AlignConstants.aimControllerP, 0, AlignConstants.aimControllerD, 
+        new TrapezoidProfile.Constraints(AlignConstants.alignMaxCorrectionSpeed, 25)
+    );
+    thetaController.enableContinuousInput(-Math.PI, Math.PI);
+    thetaController.setTolerance(AlignConstants.alignToleranceRadians);
+    addRequirements(drivetrain);
+  }
+
+  @Override
+  public void execute() {
+    Pose2d currentPose = drivetrain.getState().Pose;
+    ChassisSpeeds fieldSpeeds = drivetrain.getState().Speeds;
+
+    //latency compensation just in case, feel free to take this out
+    double lookAhead = 0.040; 
+    Pose2d futurePose = new Pose2d(
+        currentPose.getX() + (fieldSpeeds.vxMetersPerSecond * lookAhead),
+        currentPose.getY() + (fieldSpeeds.vyMetersPerSecond * lookAhead),
+        currentPose.getRotation().plus(Rotation2d.fromRadians(fieldSpeeds.omegaRadiansPerSecond * lookAhead))
+    );
+
+    Translation2d realGoal = DriverStation.getAlliance().orElse(Alliance.Blue) == Alliance.Red 
+        ? new Translation2d(FieldConstants.goalRedX, FieldConstants.goalRedY) 
+        : new Translation2d(FieldConstants.goalBlueX, FieldConstants.goalBlueY);
+
+    //iterations of time of flight
+    double refinedDist = futurePose.getTranslation().getDistance(realGoal);
+    double refinedTOF = 0;
+    Translation2d vTarget = realGoal;
+
+    for (int i = 0; i < 3; i++) {
+        refinedTOF = EquationConstants.calculateTimeOfFlight(refinedDist);
+        
+        //should this technically be a - sign?
+        vTarget = new Translation2d(
+            realGoal.getX() + (fieldSpeeds.vxMetersPerSecond * refinedTOF),
+            realGoal.getY() + (fieldSpeeds.vyMetersPerSecond * refinedTOF)
+        );
+        refinedDist = futurePose.getTranslation().getDistance(vTarget);
+    }
+
+    //final values
+    vGoalDist = refinedDist;
+    double targetAngle = MathUtil.angleModulus(
+        Math.atan2(vTarget.getY() - futurePose.getY(), vTarget.getX() - futurePose.getX()) + Math.PI
+    );
+    
+    double rotVelocity = thetaController.calculate(currentPose.getRotation().getRadians(), targetAngle);
+    double maxSpeed = TunerConstants.kSpeedAt12Volts.in(MetersPerSecond);
+
+    drivetrain.setControl(request.withSpeeds(
+        ChassisSpeeds.fromFieldRelativeSpeeds(
+            xSupplier.getAsDouble() * maxSpeed * 0.5, 
+            ySupplier.getAsDouble() * maxSpeed * 0.5, 
+            rotVelocity, 
+            currentPose.getRotation()
+        )
+    ));
+
+    AlignConstants.isAligned = thetaController.atGoal();
+  }
+}
---
+++ src/main/java/frc/robot/Commands/Shoot/AutoSOTMNew.java
@@ -0,0 +1,40 @@
+package frc.robot.Commands.Shoot;
+
+import edu.wpi.first.wpilibj2.command.Command;
+import frc.robot.Commands.Align.AlignOnTheMoveNew;
+import frc.robot.Constants.EquationConstants;
+import frc.robot.Constants.HopperConstants;
+import frc.robot.Constants.ShooterConstants;
+import frc.robot.subsystems.Hopper;
+import frc.robot.subsystems.Shooter;
+
+public class AutoSOTMNew extends Command {
+  private final Shooter shooter;
+  private final Hopper hopper;
+
+  public AutoSOTMNew(Shooter shooter, Hopper hopper) {
+    this.shooter = shooter;
+    this.hopper = hopper;
+    addRequirements(shooter, hopper);
+  }
+
+  @Override
+  public void execute() {
+    // Rev the shooter based on the final converged virtual distance
+    double targetRPS = EquationConstants.calculateRPS(AlignOnTheMoveNew.vGoalDist) + 0.5;
+    shooter.setShooterRPS(targetRPS);
+
+    if (shooter.isAtVelocity(targetRPS, ShooterConstants.kRPSTolerance)) {
+        hopper.runHopper(-HopperConstants.hopperSpeed);
+    }
+  }
+
+  @Override
+  public void end(boolean interrupted) {
+    shooter.stopAllShooters();
+    hopper.stopHopper();
+  }
+
+  @Override
+  public boolean isFinished() { return false; }
+}
